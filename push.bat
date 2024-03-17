4b02a219a9162638e4e5b5feb3189e80faf030bd
wangxin-tian <3106532244@qq.com>
2024-03-17 15:09:29 +0800

    update workflows

--- push.bat
+++ push.bat
@@ -1,12 +1,12 @@
 @REM init project
 
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:wangxin-tian/dhx-note.git
-git push -u origin main
-
+@REM git init
 @REM git add .
-@REM git commit -m "update workflows"
-@REM git push
+@REM git commit -m "first commit"
+@REM git branch -M main
+@REM git remote add origin git@github.com:wangxin-tian/dhx-note.git
+@REM git push -u origin main
+
+git add .
+git commit -m "update workflows"
+git push
